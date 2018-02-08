55dd1824ffea4edcc034f9dc1421d266544cd278
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-02-08 23:38:26 +0800

    Connect MongoDB support

--- mfutureyou/application.py
+++ mfutureyou/application.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
 
+db = MongoEngine()
 def create_app(config=None):
     app = Flask(__name__)
     if config is not None:
         app.config.from_object(config)
+
+    db.init_app(app)
 
     @app.route("/")
     def hello():
--- mfutureyou/user/views.py
+++ mfutureyou/user/views.py
@@ -1,8 +1,11 @@
 from flask import Blueprint
+from user.models import User
 
 user_page = Blueprint('user_page', __name__)
 
 @user_page.route('/login')
 def login():
-    return "This is the login page"
+    user = User(name='Bob', password='123', email='bob@email.com')
+    user.save()
+    return "Hello, {}! Your email address is {}".format(user.name, user.email)
 
