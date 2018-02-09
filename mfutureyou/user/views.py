5df1604b04ab7bae67e197f45982b4f24e4dac40
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-02-10 01:34:14 +0800

    Add sign up title for login page

--- mfutureyou/user/views.py
+++ mfutureyou/user/views.py
@@ -1,11 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from user.models import User
 
 user_page = Blueprint('user_page', __name__)
 
 @user_page.route('/login')
 def login():
-    user = User(name='Bob', password='123', email='bob@email.com')
-    user.save()
-    return "Hello, {}! Your email address is {}".format(user.name, user.email)
+    return render_template('base.html')
 
+@user_page.route('/signup')
+def signup():
+    return render_template('user/signup.html')
