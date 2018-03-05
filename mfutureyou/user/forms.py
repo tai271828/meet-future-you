f532ab66d3aa3126bf8dcbb5e19abae9910ba9fa
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-03-05 20:58:39 +0800

    fix typo

--- mfutureyou/user/forms.py
+++ mfutureyou/user/forms.py
@@ -4,7 +4,7 @@ from wtforms.fields.html5 import EmailField
 from user.models import User
 
 class RegistrationForm(FlaskForm):
-    name = StringField('Your name', [validators.DataRequired(), validators.Length(min=2, max=30))
+    name = StringField('Your name', [validators.DataRequired(), validators.Length(min=2, max=30)]
     email = EmailField('Email Address', [validators.DataRequired(), validators.Eamil()])
     password = PasswordField('New Password', [
         validators.DataRequired(),
